5d3cb78612df456d024427454409f0a38c58953e
User <User@211-07>
2020-10-15 17:43:54 +0900

    20201015: 백준 Solution_4963_섬의개수

---
+++ Algorithm/src/BaekJoon/Solution_4963_CountOfIsland.java
@@ -0,0 +1,55 @@
+package BaekJoon;
+import java.util.*;
+import java.io.*;
+public class Solution_4963_CountOfIsland {
+	public static int w,h;
+	public static int land;
+	public static int[] dx= {-1,1,0,0,1,1,-1,-1};
+	public static int[] dy= {0,0,-1,1,1,-1,1,-1};
+	public static void dfs(int i, int j, int[][] arr) {
+		arr[i][j]=0;
+		
+		for(int k=0;k<8;k++) {
+			int nx=i+dx[k];
+			int ny=j+dy[k];
+			
+			if(nx>=0 && ny>=0 && nx<h && ny<w && arr[nx][ny]==1) {
+				dfs(nx,ny,arr);
+			}
+		}
+		
+	}
+	public static void main(String[] args) throws Exception {
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+
+			w=sc.nextInt();
+			h=sc.nextInt();
+			if(w==0||h==0) {
+				break;
+			}
+			land=0;
+			int[][] arr=new int[h][w];
+			for(int i=0;i<h;i++) {
+				
+				for(int j=0;j<w;j++) {
+					arr[i][j]=sc.nextInt();
+				}
+			}
+			
+			
+			for(int i=0;i<h;i++) {
+				for(int j=0;j<w;j++) {
+					if(arr[i][j]==1) {
+						land++;
+						dfs(i,j,arr);
+					}
+				}
+			}
+			
+			System.out.println(land);
+		}
+		
+
+	}
+}
