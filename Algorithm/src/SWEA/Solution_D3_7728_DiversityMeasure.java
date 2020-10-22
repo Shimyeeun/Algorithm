18532b463f9ed522a97c7ca970e7ab3e1b5b2722
Shimyeeun <yeeun7873@naver.com>
2020-10-22 18:28:25 +0900

    20201022: SWEA Solution_D3_7728_다양성측정

---
+++ Algorithm/src/SWEA/Solution_D3_7728_DiversityMeasure.java
@@ -0,0 +1,24 @@
+package SWEA;
+import java.util.*;
+import java.io.*;
+public class Solution_D3_7728_DiversityMeasure {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int T=Integer.parseInt(br.readLine());
+		
+		for(int i=1;i<=T;i++) {
+			String s=Integer.toString(Integer.parseInt(br.readLine()));
+			System.out.println(s);
+			HashSet<Character> set=new HashSet<>();  
+			for(int j=0;j<s.length();j++) {
+				set.add(s.charAt(j));
+			}
+			
+			System.out.println("#"+i+" "+set.size());
+		}
+
+	}
+
+}
