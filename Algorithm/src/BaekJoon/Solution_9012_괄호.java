dd7010422fb5b20f4a735bca7009ad53bd1215cd
Shimyeeun <yeeun7873@naver.com>
2020-11-26 16:15:34 +0900

    20201126: 백준 Solution_9012_괄호

---
+++ Algorithm/src/BaekJoon/Solution_9012_괄호.java
@@ -0,0 +1,40 @@
+package BaekJoon;
+import java.util.*;
+import java.io.*;
+public class Solution_9012_°ýÈ£ {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int T=Integer.parseInt(br.readLine());
+		
+		for(int t=1;t<=T;t++) {
+			String s=br.readLine();
+			Stack<Character> stack=new Stack<>();
+			
+			for(int i=0;i<s.length();i++) {
+				if(stack.isEmpty()) {
+					stack.push(s.charAt(i));
+				}
+				else {
+					if(stack.peek()=='('&&s.charAt(i)==')') {
+						stack.pop();
+					} else {
+						stack.push(s.charAt(i));
+					}
+				}
+				
+			}
+			
+			if(stack.size()>0) {
+				System.out.println("NO");
+			} else {
+				System.out.println("YES");
+			}
+			
+			
+		}
+
+	}
+
+}
